0f76271933589e4ea7b200a30c09c30d0436f51c
amitrke <amitrke@gmail.com>
2018-01-20 21:54:06 -0500

    Removed Lombok

--- src/main/java/org/roorkee/demo2/Chapter.java
+++ src/main/java/org/roorkee/demo2/Chapter.java
@@ -1,21 +1,33 @@
 package org.roorkee.demo2;
 
-import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Data
 @Document
 public class Chapter {
+
     @Id
     private String id;
+
     private String name;
 
     public Chapter(String name) {
         this.name = name;
     }
 
-    /*
-    Love how the @Data annotation from lombok generates getters and setters.
-     */
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Chapter{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
